7db5e1b657810492d1820985026b590d02bbd9ec
themlprofessional <117509273+themlprofessional@users.noreply.github.com>
2023-06-12 13:57:13 +0530

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -1,1 +1,9 @@
+-- Joins
+-- A Join combines two or more tables based on a matching column.
 
+-- Types of Joins
+-- 1) Inner Join - Returns only the rows that match in both tables
+-- 2) Left Outer Join - Returns all rows from the left table, even if there are no matches in the right table
+-- 3) Right Outer Join - Returns all rows from the right table, even if there are no matches in the left table
+-- 4) Full Outer Join - Returns all rows from both tables, regardless of weather there is a match or not
+-------------------------------------------------------------------------------------------------------------------------------
